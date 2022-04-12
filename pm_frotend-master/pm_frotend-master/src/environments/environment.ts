174f6ea048148c0532d31dd50157822aadcce36e
Nikhil9949 <46377297+Nikhil9949@users.noreply.github.com>
2022-04-12 09:14:51 +0530

    Update environment.ts

--- pm_frotend-master/pm_frotend-master/src/environments/environment.ts
+++ pm_frotend-master/pm_frotend-master/src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyAxkWjXihJF1ER5PUzjS77xj18ffqfE0_4",
-    authDomain: "optimaheat-7e68a.firebaseapp.com",
-    databaseURL:"https://optimaheat-7e68a-default-rtdb.firebaseio.com/",
-    projectId: "optimaheat-7e68a",
-    storageBucket: "optimaheat-7e68a.appspot.com",
-    messagingSenderId: "680917848969",
-    appId: "1:680917848969:web:a7abe69a55482f7fc30758"
-  }
 };
 
 /*
